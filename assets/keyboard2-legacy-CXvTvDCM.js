e6756c77e73787028fe957fc201d33db2e7a7917
Jenecyl082003 <jenecylsagansan40@gmail.com>
2025-04-08 00:17:20 +0800

    Updates

--- assets/keyboard2-legacy-CXvTvDCM.js
+++ assets/keyboard2-legacy-CXvTvDCM.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C-7IQ3Sh.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-DDfkcJQ6.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
