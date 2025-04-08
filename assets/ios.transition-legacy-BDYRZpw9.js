f1a7a5372de14dd052acd69b2ce70020cc1ea14b
Jenecyl082003 <jenecylsagansan40@gmail.com>
2025-04-08 08:57:46 +0800

    Updates

--- assets/ios.transition-legacy-BDYRZpw9.js
+++ assets/ios.transition-legacy-BDYRZpw9.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DDfkcJQ6.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-Cc10Tteg.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-BfH8OAJZ.js
+++ assets/md.transition-legacy-BfH8OAJZ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DDfkcJQ6.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-Cc10Tteg.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
