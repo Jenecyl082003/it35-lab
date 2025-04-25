11847a4a5b4fd10d9e4c552087b6a357353a073c
Jenecyl082003 <jenecylsagansan40@gmail.com>
2025-04-25 19:04:02 +0800

    Updates

--- assets/ios.transition-legacy-ipqVBlJC.js
+++ assets/ios.transition-legacy-ipqVBlJC.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dwk63b6z.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-e7eeo1-r.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
