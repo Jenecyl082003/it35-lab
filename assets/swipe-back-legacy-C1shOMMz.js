a82dc1451443572e35b245c94e9f607e0320f660
Jenecyl082003 <jenecylsagansan40@gmail.com>
2025-05-03 03:12:15 +0800

    Updates

--- assets/swipe-back-legacy-C1shOMMz.js
+++ assets/swipe-back-legacy-C1shOMMz.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-e7eeo1-r.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-Ca6MPF2T.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
